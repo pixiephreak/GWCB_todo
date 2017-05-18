690d32cb75961b7d4209715fdd6a8b3afcf48fa2
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-05-17 23:54:26 -0400

    use jaws connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 var mysql = require('mysql');
 var config = require('./config');
-var connection = mysql.createConnection(config.keys);
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWS_URL);
+}else{
+  connection = mysql.createConnection(config.keys);
+}
 
 connection.connect(function(err) {
   if (err) {
